b926bc59d3cb0ca9f1dc7693e3f9de97842ca15f
NoaLeGeek <noacraft68@gmail.com>
2024-02-11 21:00:17 +0100

    euhhhh trying

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import json
-import requests
+import os
+#import requests
 
 url = "https://neal.fun/api/infinite-craft/pair"
+default_elements = [{"🌬️ Wind": False}, {"🔥 Fire": False}, {"💧 Water": False}, {"🌍 Earth": False}]
 
 def main():
-    pass
-
+    while len(input("First element:\t")) > 1:
+        elements = []
+        if os.path.exists("elements.json"):
+            with open("elements.json", "r") as file:
+                data = json.load(file)
+                elements = data["elements"]
+            print([str(list(e.keys())) for e in default_elements])
+            if not all(e.keys() for e in elements):
+              with open("elements.json", "w") as file:
+                json.dump({"elements": default_elements}, file)  
+        else:
+            create_elements()
+                
+def create_elements():
+    with open("elements.json", "w") as file:
+                json.dump({"elements": default_elements}, file)
 
 if __name__ == "__main__":
     main()
